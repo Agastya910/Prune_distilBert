14e682c4ce89d06e51518b3dd4c5f997fac344d5
Agastya Todi <76257446+Agastya910@users.noreply.github.com>
2025-03-26 15:27:00 -0500

    Update manual_prune_2.py

--- manual_prune_2.py
+++ manual_prune_2.py
@@ -4,29 +4,7 @@ from transformers import AutoModelForSequenceClassification
 from config import MODEL_SAVE_PATH, PRUNE_PERCENTAGE
 
 from utils import GradientCollector
-class GradientCollector:
-    def __init__(self, model):
-        self.model = model
-        self.gradients = {}
-        self.hooks = []
-        
-        # Register full backward hooks on transformer layers
-        for name, module in self.model.named_modules():
-            if 'transformer.layer' in name:
-                hook = module.register_full_backward_hook(
-                    self._store_grad_hook(name)
-                )
-                self.hooks.append(hook)
-    
-    def _store_grad_hook(self, name):
-        def hook(module, grad_input, grad_output):
-            # Store the L2 norm of the output gradients
-            self.gradients[name] = grad_output[0].norm().item()
-        return hook
-    
-    def remove_hooks(self):
-        for hook in self.hooks:
-            hook.remove()
+
 
 def prune_transformer_layers(model, layers_to_prune):
     """Remove specified layers from transformer"""
